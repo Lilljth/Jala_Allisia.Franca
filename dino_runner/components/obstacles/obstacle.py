dcd37449ce808e10af229c3429983376cc678b4e
Lilljth <90848823+Lilljth@users.noreply.github.com>
2023-06-20 18:52:22 -0300

    obstáculos

--- dino_runner/components/obstacles/obstacle.py
+++ dino_runner/components/obstacles/obstacle.py
@@ -2,6 +2,8 @@ import pygame
 from pygame.sprite import Sprite
 
 from dino_runner.utils.constants import SCREEN_WIDTH
+
+
 class Obstacle(Sprite):
     def __init__(self,image,type):
         self.type = type
--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,16 +1,21 @@
+import random
 import pygame
 
-from dino_runner.components.obstacles.cactus import Cactus
-from dino_runner.utils.constants import SMALL_CACTUS
-
-
+from dino_runner.components.obstacles.cactus import Cactus, Large_Cactus
+from dino_runner.components.obstacles.bird import Bird
+from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS, BIRD
 class ObstacleManager:
     def __init__(self):
         self.obstacles = []
 
     def update(self, game):
         if len(self.obstacles) == 0:
-            self.obstacles.append(Cactus(SMALL_CACTUS))
+            if random.randint(0,2) == 0:
+                self.obstacles.append(Cactus(SMALL_CACTUS))
+            elif random.randint(0,2) == 1:
+                self.obstacles.append(Large_Cactus(LARGE_CACTUS))
+            elif random.randint(0,2) == 2:
+                self.obstacles.append(Bird(BIRD))
 
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed,self.obstacles)
